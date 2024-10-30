6fef4c48a7cd229eef8eaaa281c7b21af6794215
blujai831 <webmaster@blujai831.dev>
2024-10-30 14:30:11 -0700

    Move candle logic into new base class CarriableItem, allowing for carrying other items at some future time. (We will not need this yet or possibly ever, but just in case.)

---
+++ objects/carriable_item.gd
@@ -0,0 +1,33 @@
+class_name CarriableItem
+extends RigidBody2D
+
+@onready var _player: PlayerCharacter = $'../PlayerCharacter'
+@onready var _pick_area = $'PickArea'
+
+var in_contact = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_pick_area.body_entered.connect(_on_body_entered)
+	_pick_area.body_exited.connect(_on_body_exited)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	if _player.heldItem == self:
+		global_position = _player.global_position + Vector2.UP*32
+		linear_velocity = _player.get_real_velocity()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_released("pick_up"):
+		if _player.heldItem == self:
+			_player.heldItem = null
+		elif in_contact and _player.heldItem == null:
+			_player.heldItem = self
+
+func _on_body_entered(body: Node2D):
+	if body == _player:
+		in_contact = true
+
+func _on_body_exited(body: Node2D):
+	if body == _player:
+		in_contact = false
