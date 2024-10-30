f589a399ed73d204846ae1583a1252deb53e7616
blujai831 <webmaster@blujai831.dev>
2024-10-30 12:58:23 -0700

    Refactor out repeated calls to get_node in candle.gd.

--- objects/candle.gd
+++ objects/candle.gd
@@ -1,6 +1,8 @@
 class_name Candle
 extends RigidBody2D
 
+@onready var _player = $'../PlayerCharacter'
+
 var picked = false
 
 # Called when the node enters the scene tree for the first time.
@@ -10,14 +12,14 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	if picked:
-		self.position = get_node("../PlayerCharacter/Marker2D").global_position
+		self.position = _player.global_position
 
 func _input(event: InputEvent) -> void:
 	if(Input.is_key_pressed(KEY_E)):
 		if picked:
 			picked = false
-			get_node("../PlayerCharacter").canPick = true
+			_player.canPick = true
 		else:
-			if get_node("../PlayerCharacter").canPick == true:
+			if _player.canPick:
 				picked = true
-				get_node("../PlayerCharacter").canPick = false
+				_player.canPick = false
