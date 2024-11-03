3e206054326febdd962228a26ab425ac07e3bbee
blujai831 <webmaster@blujai831.dev>
2024-11-03 05:37:11 -0800

    Move candle-specific logic into candle.gd

--- objects/candle.gd
+++ objects/candle.gd
@@ -1,2 +1,16 @@
 class_name Candle
 extends CarriableItem
+
+@onready var _collision_light = $CollisionEnablingLight
+
+# Initialize the candle with specific properties
+func initialize(scale_factor: float) -> void:
+	if _collision_light:
+		_collision_light.scale *= scale_factor
+
+func _get_scale() -> Vector2:
+	return _collision_light.scale
+
+func _set_scale(_new_scale: Vector2):
+	if _collision_light:
+		_collision_light.scale = _new_scale
--- objects/carriable_item.gd
+++ objects/carriable_item.gd
@@ -3,7 +3,6 @@ extends RigidBody2D
 
 @onready var _player: PlayerCharacter = $'../PlayerCharacter'
 @onready var _pick_area = $'PickArea'
-@onready var _collision_light = $CollisionEnablingLight
 
 var in_contact = false
 
@@ -11,18 +10,6 @@ var in_contact = false
 func _ready() -> void:
 	_pick_area.body_entered.connect(_on_body_entered)
 	_pick_area.body_exited.connect(_on_body_exited)
-
-# Initialize the candle with specific properties
-func initialize(scale_factor: float) -> void:
-	if _collision_light:
-		_collision_light.scale *= scale_factor
-
-func _get_scale() -> Vector2:
-	return _collision_light.scale
-
-func _set_scale(_new_scale: Vector2):
-	if _collision_light:
-		_collision_light.scale = _new_scale
 
 func _physics_process(_delta: float) -> void:
 	if _player.heldItem == self:
